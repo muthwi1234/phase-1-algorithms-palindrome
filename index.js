bfb65c5b41b77a65b8d3c3688f938de46e753d81
Peter <peterkilungya1@gmail.com>
2022-08-29 12:20:54 +0300

    assignment completed

--- index.js
+++ index.js
@@ -23,3 +23,31 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+function isPalindrome(word) {
+  // Write your algorithm here
+ const reversedWord = reverse(word)
+  return word === reversedWord;
+}
+/*
+  Add your pseudocode here
+  if the word is the same as reverse word,It should return true.
+  reverse the input string
+  if the input string is same as the reversed input
+  return true else return false
+*/
+/*
+  Add written explanation of your solution here
+*/
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
+module.exports = isPalindrome;
